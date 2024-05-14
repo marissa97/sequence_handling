351848da7f9b73abc97d3d506537db62173f4fe0
marissa97 <65372295+marissa97@users.noreply.github.com>
2024-05-14 13:33:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,19 @@ from main import *
 Test scenario: 
 - creating db with fastq files in db input folder (dbFolder)
 - filtering with input_fastq1 and input_fastq2 
+
+Args:
+    input_fastq1 (str): Path to the first input FASTQ file.
+    input_fastq2 (str): Path to the second input FASTQ file.
+    threshold (int): Threshold value for filtering sequences.
+    output_fasta (str): Path to the output FASTA file.
+    outdir (str): Directory to store output files.
+    db_name (str): Name of the database.
+    db_folder (str, optional): Path to the database folder. Defaults to None.
+    min_overlap(int, default:200): Minimum overlap length 
+    min_amplicon_length(int, default:400): Minimum Amplicon length
+    fixed_sequence1 (str, optional): Fixed sequence for input FASTQ 1. Defaults to None.
+    fixed_sequence2 (str, optional): Fixed sequence for input FASTQ 2. Defaults to None.
 '''
 
 dbFolder= "/home/marissa/Documents/dkms/input_DB"
